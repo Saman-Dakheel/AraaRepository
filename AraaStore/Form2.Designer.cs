48c5b4e516e8670591c1075d5aa7bbfc20d55db7
Ahmedkhalil98 <ahmedkhlil756@gmail.com>
2022-09-25 17:56:10 +0300

    update form 2

--- AraaStore/Form2.Designer.cs
+++ AraaStore/Form2.Designer.cs
@@ -30,6 +30,8 @@ namespace AraaStore
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +44,33 @@ namespace AraaStore
             this.label1.TabIndex = 0;
             this.label1.Text = "by Ahmed";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(511, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "by Ahmed";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(116, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "by Ahmed";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -58,5 +82,7 @@ namespace AraaStore
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
