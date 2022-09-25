7ef3ef11ca252ea3063e51f2a45493631d9c8f16
Ahmedkhalil98 <ahmedkhlil756@gmail.com>
2022-09-25 18:00:31 +0300

    btnnnnn

--- AraaStore/NewSales.Designer.cs
+++ AraaStore/NewSales.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // newsales
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // NewSales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Enabled = false;
-            this.Name = "newsales";
+            this.Name = "NewSales";
             this.Text = "newsales";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
